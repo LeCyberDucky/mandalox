8f3baec4a0d5e78c7f524f51dea1fe8cf2bef699
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2020-04-01 17:35:39 +0200

    To infinity... and beyond!

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+fn main() {
+
+}
+
+#[derive(Debug)]
+struct Triangle {
+    corners: Vec<Point>,
+}
+
+#[derive(Debug)]
+struct Point {
+    x: u16,
+    y: u16,
+}
+
+
+// Edge detection
+// Edge simplification
